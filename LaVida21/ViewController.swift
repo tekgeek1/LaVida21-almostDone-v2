9805269b14560aacd7d22b42f8df10b6b00566ee
Nathan Simmons <nathansimmons@nathans-macbook-pro-2.local>
2018-12-13 23:30:40 -0500

    Initial Commit

---
+++ LaVida21/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LaVida21
+//
+//  Created by Nathan Simmons on 12/13/18.
+//  Copyright © 2018 Nathan Simmons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
